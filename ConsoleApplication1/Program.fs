3b30ab0d2a1049b7f3cdf42b679c01f3e36062f3
Rumyana Neykova <rumyana.neykova10@imperial.ac.uk>
2017-11-11 14:55:15 +0000

    Remove Result

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -1,4 +1,24 @@
-﻿module Helpers =
+﻿
+open System.IO
+open System.Diagnostics
+
+module TimeMeasure = 
+    let mutable stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let path = "C:/Users/rn710/Repositories/GenerativeTypeProviderExample/Evaluation/"
+    let file = "C:/Users/rn710/Repositories/GenerativeTypeProviderExample/Evaluation/tempfib.txt"
+
+    let start() = 
+        stopWatch.Stop()
+        stopWatch <- System.Diagnostics.Stopwatch.StartNew()
+
+    let measureTime (step:string) = 
+        stopWatch.Stop()
+        let numSeconds = stopWatch.ElapsedTicks / Stopwatch.Frequency
+        let curTime = sprintf "%s: %i \r\n" step stopWatch.ElapsedMilliseconds
+        File.AppendAllText(file, curTime)
+        stopWatch <- Stopwatch.StartNew()
+
+module Helpers =
 
     let sep = ":"B
     let sepEnd = "."B
@@ -8,7 +28,7 @@
     type IFailure =
         abstract member Description: string
 
-    [<NoComparison>]
+   (* [<NoComparison>]
     [<NoEquality>]
     type Result<'a> =
         | Success of 'a
@@ -21,6 +41,9 @@
         member x.Result =
             let (ResultWrapped f) = x
             f()
+
+
+
 
     type ResultBuilder() =
         member __.Bind(ResultWrapped m, f) =
@@ -45,16 +68,16 @@
         member __.Run(delayed) = ResultWrapped delayed
 
             
-    let result = ResultBuilder()
+    let result = ResultBuilder()*)
 
 
-    let createFailure (failure:#IFailure) = 
-        fun() -> Failure failure
-        |> ResultWrapped
+    //let createFailure (failure:#IFailure) = 
+     //   fun() -> Failure failure
+        //|> ResultWrapped
 
-    let run (oct:ResultWrapped<'a>) = 
+    (*let run (oct:ResultWrapped<'a>) = 
         let (ResultWrapped oct) = oct
-        oct() 
+        oct() *)
 
     type Failure =
         | Deserialization of string
@@ -84,28 +107,25 @@ module Types =
         | C
 
 module Labels =
-    open Types
+//    open Types
     open System
     open Helpers
     open System.IO
 
     let private readData (input:byte[]) (data:byte[]) =
-        result{
             let index = input.Length 
             let head,tail= Array.splitAt index data
             if input = head then
-                return tail
+                tail
             else
-                return! Deserialization (sprintf "Expected %A but received %A" input head ) |> createFailure
-        }
+                failwith "Expected %A but received %A" input head
+                //Deserialization (sprintf "Expected %A but received %A" input head ) //|> createFailure
 
 
     let readInt (data:byte[]) = 
-        result{
             let index = 4
             let head,tail= Array.splitAt index data
-            return (BitConverter.ToInt32(head,0),tail)
-        }
+            (BitConverter.ToInt32(head,0),tail)
 
 
     let readHello = readData "HELLO"B        
@@ -124,7 +144,7 @@ module Labels =
     type System.Int32 with  
         member x.Send(stream:BinaryWriter) = stream.Write x
 
-        static member Receive(stream:BinaryReader) = result{ return stream.ReadInt32() }
+        static member Receive(stream:BinaryReader) = stream.ReadInt32()
         
     type Hello = 
         | Hello of int
@@ -136,15 +156,13 @@ module Labels =
             stream.Write sepEnd
 
         static member Receive(stream:BinaryReader) =
-            result{
                 let data = stream.ReadBytes(11)
-                printfn "%A" data
-                let! data = readHello data
-                let! data = readSep data
-                let! value,data = readInt data
-                let! data = readSepEnd data
-                return Hello value
-            }
+                //printfn "%A" data
+                let data = readHello data
+                let data = readSep data
+                let value,data = readInt data
+                let data = readSepEnd data
+                Hello value
 
     type Add = 
         | Add of int 
@@ -156,14 +174,11 @@ module Labels =
             stream.Write sepEnd
 
         static member Receive(stream:BinaryReader) =
-            result{
                 let data = stream.ReadBytes(6)
-
-                let! data = readSep data
-                let! value,data = readInt data
-                let! data = readSepEnd data
-                return Add value
-            }
+                let data = readSep data
+                let value,data = readInt data
+                let data = readSepEnd data
+                Add value
         
     type Bye = 
         | Bye
@@ -172,12 +187,9 @@ module Labels =
             stream.Write sepEnd
 
         static member Receive(stream:BinaryReader) =
-            result{
                 let data = stream.ReadBytes(1)
-
-                let! data = readSepEnd data
-                return Bye
-            }
+                let data = readSepEnd data
+                Bye
 
     type Choice =
         | ChoiceBye of Bye
@@ -188,17 +200,16 @@ module Labels =
             | ChoiceAdd add -> add.Send(stream)
 
         static member Receive(stream:BinaryReader) =
-            result{
                 let data = stream.ReadBytes(3)
                 if data = "BYE"B then
-                    let! bye = Bye.Receive(stream) 
-                    return bye |> ChoiceBye
+                    let bye = Bye.Receive(stream) 
+                    bye |> ChoiceBye
                 elif data = "ADD"B then
-                    let! add = Add.Receive(stream)
-                    return add |> ChoiceAdd
+                    let add = Add.Receive(stream)
+                    add |> ChoiceAdd
                 else
-                    return! Deserialization (sprintf "Expected %A or %A but received %A" "BYE"B "ADD"B data) |> createFailure
-            }
+                    failwith "Expected %A or %A but received %A" "BYE"B "ADD"B data
+                    //Deserialization (sprintf "Expected %A or %A but received %A" "BYE"B "ADD"B data) //|> createFailure
 
 
     type Res = 
@@ -211,104 +222,102 @@ module Labels =
             stream.Write sepEnd
 
         static member Receive(stream:BinaryReader) =
-            result{
+            
                 let data = stream.ReadBytes(9)
 
-                let! data = readRes data
-                let! data = readSep data
-                let! value,data = readInt data
-                let! data = readSepEnd data
-                return Res value
-            }
+                let data = readRes data
+                let data = readSep data
+                let value,data = readInt data
+                let data = readSepEnd data
+                Res value
 
 module FibServer =
     open System.Net
     open System.Net.Sockets
-    open Types
+   // open Types
     open Labels
     open Helpers 
     open System.IO
     
 
-    let rec executeServer (previous:int) (acc:int) (writeStream:NetworkStream) (readStream:NetworkStream) =
-        result{
-            let reader = new BinaryReader(readStream)
-            let writer = new BinaryWriter(writeStream)
-            printfn "start"
-            let! hello = Hello.Receive(reader)
-            printfn "start1"
-            let! choice = Choice.Receive(reader)
-            printfn "start2"
+    let rec executeServer (previous:int) (acc:int) (writer:BinaryWriter) (reader:BinaryReader) =
+            //printfn "start"
+            let hello = Hello.Receive(reader)
+            //printfn "start1"
+            let choice = Choice.Receive(reader)
+            //printfn "start2"
             match choice with
             | ChoiceBye _ ->
-                do Bye.Bye.Send(writer)
-                return acc
+                Bye.Bye.Send(writer)
+                acc
             | ChoiceAdd (Add value) ->
-                let acc1 = previous + value
-                Res.Res(acc1).Send(writer)
-                return! executeServer value acc1 writeStream readStream
-         }
+                //let acc1 = previous + value
+                let ass = if value > 0 
+                          then 0  else 1  
+                Res.Res(value).Send(writer)
+                executeServer value value writer reader
           
     let agentServ (port) =
         new MailboxProcessor<unit>(fun inbox ->
             let rec loop() = 
                 async {
-                    printfn "Start"
+                    //printfn "Start"
                     let! msg = inbox.Receive()
-                    printfn "after"
-
+                    //printfn "after"
                     let server = new TcpListener(IPAddress.Parse("127.0.0.1"),5000)
                     server.Start()
                     let readStream = server.AcceptTcpClient().GetStream()
                     let writeStream = 
                         let clt = new TcpClient("127.0.0.1",5001)
                         clt.GetStream()
-                    printfn "test"
-
-                    let result = executeServer 1 1 writeStream readStream
-                    match run result with
-                    | Failure failure -> printfn "%s" failure.Description
-                    | Success success -> printfn "Server Done : Sent value %i " success
+                    //printfn "test"
+                    let reader = new BinaryReader(readStream)
+                    let writer = new BinaryWriter(writeStream)
+                    let hello = Hello.Receive(reader)
+                    Hello.Hello(0).Send(writer)
+                    let result = executeServer 1 1 writer reader
+                    printfn "Server Done : Sent value %i " result
+                    //match result with
+                    //| Failure failure -> printfn "%s" failure.Description
+                    //| Success success -> printfn "Server Done : Sent value %i " success
                 }
             loop()
         ) 
 
-    let agentServer = agentServ 5000
-    agentServer.Start()
-    agentServer.Post()
+    let StartServer () = 
+        let agentServer = agentServ 5000
+        agentServer.Start()
+        agentServer.Post()
 
 module FibClient =
     open System.Net
     open System.Net.Sockets
-    open Types
+   // open Types
     open Labels
     open Helpers 
     open System.IO
-    
-    let rec executeClient (iter:int) (acc:int) (writeStream:NetworkStream) (readStream:NetworkStream) =
-        result{
-            let reader = new BinaryReader(readStream)
-            let writer = new BinaryWriter(writeStream)
-            
-            do Hello.Hello(0).Send(writer)
+    //NetworkStream
+    let rec executeClient (iter:int) (acc:int) (writer:BinaryWriter) (reader:BinaryReader) =
+            Hello.Hello(0).Send(writer)
+            //printf "Iter: %i" iter
             if iter = 0 then
                 do Bye.Bye.Send(writer)
                 let data = reader.ReadBytes(3)
-                let! data = readBye data
-                let! bye = Bye.Receive(reader)
-                return acc
+                let data = readBye data
+                let bye = Bye.Receive(reader)
+                acc
             else
-                do Add.Add(acc).Send(writer)
-                let! (Res res) = Res.Receive(reader)
-                return! executeClient (iter - 1) res writeStream readStream
-         }
+                Add.Add(acc).Send(writer)
+                let (Res res) = Res.Receive(reader)
+                executeClient (iter - 1) res writer reader
+         
 
 
     let agentClt (port) =
         new MailboxProcessor<unit>(fun inbox ->
             let rec loop() = 
                 async {
-                    let! msg = inbox.Receive()
+                    let msg = inbox.Receive()
 
                     let writeStream = 
                         let clt = new TcpClient("127.0.0.1",5000)
@@ -316,26 +325,39 @@ module FibClient =
                     let server = new TcpListener(IPAddress.Parse("127.0.0.1"),5001)
                     server.Start()
                     let readStream = server.AcceptTcpClient().GetStream()
-                    printfn "test"
-                        
-                    let result = executeClient 10 1 writeStream readStream
-                    match run result with
-                    | Failure failure -> printfn "%s" failure.Description
-                    | Success success -> printfn "Client Done : Received value %i " success
+                    //printfn "test"
+                    let reader = new BinaryReader(readStream)
+                    let writer = new BinaryWriter(writeStream)
+                    Hello.Hello(0).Send(writer)
+                    let hello = Hello.Receive(reader)
+
+                    TimeMeasure.start() 
+                    TimeMeasure.measureTime "before TCP start"
+                    let result = executeClient 10000 1 writer reader
+                    //match result with
+                    printfn "Client Done : Received value %i " result
+                    TimeMeasure.measureTime "TCP time"
+                    (*| Failure failure -> printfn "%s" failure.Description
+                    | Success success -> 
+                        //printfn "Client Done : Received value %i " success
+                        TimeMeasure.measureTime "TCP time"*)
                 }
             loop()
         ) 
-
-    let agentClient = agentClt 5001
-    agentClient.Start()
-    agentClient.Post()
-
-
-
+    let startClinet () = 
+        let agentClient = agentClt 5001
+        agentClient.Start()
+        agentClient.Post()
 
 
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
-    0 // return an integer exit code
+    if argv.[0] = "C" then 
+        printfn "Starting the client"
+        FibClient.startClinet ()
+    else
+        printfn "Starting the client" 
+        FibServer.StartServer ()
+    System.Console.Read() // return an integer exit code
